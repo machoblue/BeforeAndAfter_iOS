399b032afa7ebfb94946861ed3fb89e6a962ce68
松島勇貴 <macho.macho.dev@gmail.com>
2020-03-01 08:10:29 +0900

    Refactoring

--- BeforeAndAfter/UIView/LineChartView.swift
+++ BeforeAndAfter/UIView/LineChartView.swift
@@ -14,6 +14,24 @@ class LineChartView: UIView {
     let marginTrailing = BAMargin.medium
     let marginBottom = BAMargin.medium
     
+    // Chart size
+    var width: CGFloat = 0
+    var height: CGFloat = 0
+    var graphWidth: CGFloat = 0
+    var graphHeight: CGFloat = 0
+    var graphOffsetX: CGFloat = 0
+    var graphOffsetY: CGFloat = 0
+
+    // YAxis range
+    var weightLowerLimit: Float = 0
+    var weightUpperLimit: Float = 0
+    var fatPercentLowerLimit: Float = 0
+    var fatPercentUpperLimit: Float = 0
+    
+    // XAxis range
+    var fromTime: TimeInterval = 0
+    var toTime: TimeInterval = 0
+    
     var mode: ChartRange = .threeWeeks {
         didSet {
             self.setNeedsLayout()
@@ -37,15 +55,23 @@ class LineChartView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
-
-        let width = rect.width
-        let height = rect.height
+        width = rect.width
+        height = rect.height
         let xAxisLabelHeight = height * 0.05
-        let graphOffsetX = self.marginLeading
-        let graphOffsetY = self.marginTop
-        let graphWidth = width - (self.marginLeading + self.marginTrailing)
-        let graphHeight = height - (xAxisLabelHeight + self.marginTop + self.marginBottom)
+        graphOffsetX = self.marginLeading
+        graphOffsetY = self.marginTop
+        graphWidth = width - (self.marginLeading + self.marginTrailing)
+        graphHeight = height - (xAxisLabelHeight + self.marginTop + self.marginBottom)
+        
+        drawChartFrame(rect: rect)
+        
+        calculateYAxisRange()
+        calculateXAxisRange()
+        
+        drawWeightChart(rect: rect)
+    }
 
+    private func drawChartFrame(rect: CGRect) {
         let path = UIBezierPath()
         path.move(to: CGPoint(x: graphOffsetX, y: graphOffsetY))
         path.addLine(to: CGPoint(x: graphOffsetX, y: graphOffsetY + graphHeight)) // Y Axis1
@@ -53,17 +79,20 @@ class LineChartView: UIView {
         path.addLine(to: CGPoint(x: graphOffsetX + graphWidth, y: graphOffsetY)) // YAxis 2
         UIColor.black.set()
         path.stroke()
-        
+    }
+    
+    private func calculateYAxisRange() {
         let sortedByWeight = self.records.sorted { ($0.weight ?? 0) < ($1.weight ?? 0) }
         let weightMax = sortedByWeight.last?.weight ?? 0
         let weightMin = sortedByWeight.first?.weight ?? 0
         let sortedByFatPercent = self.records.sorted { ($0.fatPercent ?? 0) < ($1.fatPercent ?? 0) }
         let fatPercentMax = sortedByFatPercent.last?.fatPercent ?? 0
         let fatPercentMin = sortedByFatPercent.first?.fatPercent ?? 0
-        var weightUpperLimit = ceil(weightMax) + 1
-        var weightLowerLimit = floor(weightMin) - 1
-        var fatPercentUpperLimit = ceil(fatPercentMax) + 1
-        var fatPercentLowerLimit = floor(fatPercentMin) - 1
+        
+        weightUpperLimit = ceil(weightMax) + 1
+        weightLowerLimit = floor(weightMin) - 1
+        fatPercentUpperLimit = ceil(fatPercentMax) + 1
+        fatPercentLowerLimit = floor(fatPercentMin) - 1
         let weightRange = weightUpperLimit - weightLowerLimit
         let fatPercentRange = fatPercentUpperLimit - fatPercentLowerLimit
         if (weightRange > fatPercentRange) {
@@ -73,16 +102,22 @@ class LineChartView: UIView {
             weightLowerLimit = weightLowerLimit - Float(Int(fatPercentRange - weightRange) / 2)
             weightUpperLimit = weightLowerLimit + fatPercentRange
         }
-        
-        let offset = Double(60 * 60 * 24)
-        let toTime = Date().timeIntervalSince1970 + offset
-        let fromTime = toTime - (self.mode.time + offset)
-
+    }
+    
+    private func calculateXAxisRange() {
+//        let offset = Double(60 * 60 * 24)
+        let offset = self.mode.time * 0.05
+        toTime = Date().timeIntervalSince1970 + offset
+        fromTime = toTime - (self.mode.time + offset)
+    }
+    
+    private func drawWeightChart(rect: CGRect) {
         let lineChartPath = UIBezierPath()
         for (index, record) in self.records.enumerated() {
             guard let weight = record.weight else { return }
-            let x = self.marginLeading + graphWidth * CGFloat((record.time - fromTime) / (toTime - fromTime))
-            let y = self.marginTop + graphHeight * CGFloat((weightUpperLimit - weight) / (weightUpperLimit - weightLowerLimit))
+            let x = self.marginLeading + self.graphWidth * CGFloat((record.time - fromTime) / (toTime - fromTime))
+            
+            let y = self.marginTop + self.graphHeight * CGFloat((self.weightUpperLimit - weight) / (self.weightUpperLimit - self.weightLowerLimit))
             let point = CGPoint(x: x, y: y)
             if record.time >= fromTime, record.time <= toTime {
                 if index == 0 { // 直前のレコードがない
@@ -100,8 +135,8 @@ class LineChartView: UIView {
                 }
             }
         }
+        
         UIColor.red.set()
         lineChartPath.stroke()
     }
-
 }
