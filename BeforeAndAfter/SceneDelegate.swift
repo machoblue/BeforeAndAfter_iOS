2a6684f536b659bb3879531583a66bd4791bf507
松島勇貴 <macho.macho.dev@gmail.com>
2020-03-09 07:52:13 +0900

    Show TargetView on first launching

--- BeforeAndAfter/SceneDelegate.swift
+++ BeforeAndAfter/SceneDelegate.swift
@@ -19,13 +19,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Create the SwiftUI view that provides the window contents.
-        let tabView = BATabView()
-
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: tabView)
+            window.rootViewController = UserDefaults.haveLaunchAppBefore ? UIHostingController(rootView: BATabView()) : UIHostingController(rootView: TargetView())
             self.window = window
             window.makeKeyAndVisible()
         }
--- BeforeAndAfter/View/TargetView.swift
+++ BeforeAndAfter/View/TargetView.swift
@@ -8,44 +8,49 @@
 import SwiftUI
 
 struct TargetView: View {
+    @ObservedObject var viewModel: TargetViewModel
     @State var weightTarget: String = ""
     @State var fatPercentTarget: String = ""
+    @State var presentTabView = false
+    
+    init(viewModel: TargetViewModel = TargetViewModel()) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
         VStack {
+            Spacer()
             Text("インストールありがとうございます。")
-            Spacer()
-                .frame(width: 10, height: 24)
             Text("まず、目標を入力してください。")
-            
-            Spacer()
-                .frame(width: 10, height: 48)
-            
+                .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
+
             HStack {
                 TextField("目標体重", text: $weightTarget)
                     .frame(width: 160)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 Text("kg")
             }
-            
-            Spacer()
-                .frame(width: 10, height: 24)
-            
+            .padding(EdgeInsets(top: 48, leading: 0, bottom: 0, trailing: 0))
+
             HStack {
                 TextField("目標体脂肪率", text: $fatPercentTarget)
                     .frame(width: 160)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 Text("%")
             }
-            
+            .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
+
             Spacer()
                 .frame(width: 10, height: 64)
 
             Button(action: {
-                
+                self.viewModel.apply(.onSaveButtonTapped(weightTarget: Float(self.weightTarget) ?? 0, fatPercentTarget: Float(self.fatPercentTarget) ?? 0))
+                UserDefaults.haveLaunchAppBefore = true
+                self.presentTabView = true
             }) {
                 HStack {
                     Spacer()
-                    Text("次へ")
+                    Text("保存")
                     Spacer()
                 }
                 .padding(EdgeInsets(top: 12, leading: 0, bottom: 12, trailing: 0))
@@ -55,13 +60,17 @@ struct TargetView: View {
             .cornerRadius(8)
 
             Button(action: {
-                
+                UserDefaults.haveLaunchAppBefore = true
+                self.presentTabView = true
             }) {
                 Text("スキップ")
             }
             .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
+            
+            Spacer()
         }
         .padding(EdgeInsets(top: 0, leading: 36, bottom: 0, trailing: 36))
+        .overlay(presentTabView ? BATabView() : nil)
     }
 }
 
