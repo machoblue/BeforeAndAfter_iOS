35ed8b50cfd854ef8438faccca6ddcb95c68611f
松島勇貴 <macho.macho.dev@gmail.com>
2020-03-10 23:21:14 +0900

    add TUtorialView

--- BeforeAndAfter/View/TutorialView.swift
+++ BeforeAndAfter/View/TutorialView.swift
@@ -10,13 +10,32 @@ import SwiftUI
 struct TutorialView: View {
     @Binding var presentTutorial: Bool
     
+    let diameter: CGFloat = 100 // 直径
+    
     var body: some View {
-        Rectangle()
-            .foregroundColor(Color(UIColor.black.withAlphaComponent(0.5)))
-            .gesture(TapGesture().onEnded {
-//                UserDefaults.haveLaunchAppBefore = true
-                self.presentTutorial = false
-            })
+        GeometryReader { [diameter] geometry in
+            RectangleWithCircle(circleRect: CGRect(x: geometry.size.width - 85, y: -10, width: diameter, height: diameter))
+                .fill(Color(UIColor.black.withAlphaComponent(0.5)), style: FillStyle(eoFill: true))
+                .edgesIgnoringSafeArea(.top)
+            .gesture(
+                TapGesture().onEnded {
+                    // UserDefaults.haveLaunchAppBefore = true // TODO: uncomment
+                    self.presentTutorial = false
+                }
+            )
+        }
+    }
+}
+
+struct RectangleWithCircle: Shape {
+    let circleRect: CGRect
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addRect(rect)
+        let circleHole = Path(roundedRect: circleRect, cornerSize: CGSize(width: circleRect.width / 2, height: circleRect.height / 2))
+        path.addPath(circleHole)
+        
+        return path
     }
 }
 
