895a5677a8a711115925b58aa4fd321fa97712a7
松島勇貴 <macho.macho.dev@gmail.com>
2020-03-15 08:37:01 +0900

    Add message bubble

--- BeforeAndAfter/View/TutorialView.swift
+++ BeforeAndAfter/View/TutorialView.swift
@@ -13,16 +13,29 @@ struct TutorialView: View {
     let diameter: CGFloat = 100 // 直径
     
     var body: some View {
-        GeometryReader { [diameter] geometry in
-            RectangleWithCircle(circleRect: CGRect(x: geometry.size.width - 85, y: -10, width: diameter, height: diameter))
-                .fill(Color(UIColor.black.withAlphaComponent(0.5)), style: FillStyle(eoFill: true))
-                .edgesIgnoringSafeArea(.top)
-            .gesture(
-                TapGesture().onEnded {
-                    // UserDefaults.haveLaunchAppBefore = true // TODO: uncomment
-                    self.presentTutorial = false
+        GeometryReader { geometry in
+            ZStack {
+                RectangleWithCircle(circleRect: CGRect(x: geometry.size.width - 85, y: -10, width: self.diameter, height: self.diameter))
+                    .fill(Color(UIColor.black.withAlphaComponent(0.7)), style: FillStyle(eoFill: true))
+                    .edgesIgnoringSafeArea(.top)
+                .gesture(
+                    TapGesture().onEnded {
+                         UserDefaults.haveLaunchAppBefore = true
+                        self.presentTutorial = false
+                    }
+                )
+                
+                VStack {
+                    Spacer()
+                        .frame(height: 110)
+                    Text("まずは右上のボタンから、\nいまの体重を記録してみましょう！")
+                        .font(.system(size: BAFontSize.large))
+                        .frame(alignment: .center)
+                        .padding(16)
+                        .background(MessageBubble())
+                    Spacer()
                 }
-            )
+            }
         }
     }
 }
@@ -36,6 +49,33 @@ struct RectangleWithCircle: Shape {
         path.addPath(circleHole)
         
         return path
+    }
+}
+
+struct MessageBubble: View {
+    let padding: CGFloat = 16
+    let cornerRadius: CGFloat = 8
+    let tailWidth: CGFloat = 16
+    let tailHeight: CGFloat = 16
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: self.cornerRadius)
+                    .foregroundColor(.white)
+                VStack {
+                    Path { path in
+                        path.move(to: CGPoint(x: geometry.size.width - self.cornerRadius - (self.tailWidth / 2), y: -self.padding)) // 三角の先っちょ。ここから時計回りに三角形を描く
+                        path.addLine(to: CGPoint(x: geometry.size.width - self.cornerRadius - 6 /* 6: offset */, y: 0))
+                        path.addLine(to: CGPoint(x: geometry.size.width - self.cornerRadius - self.tailWidth - 6 /* 6: offset */, y: 0))
+                        path.addLine(to: CGPoint(x: geometry.size.width - self.cornerRadius - (self.tailWidth / 2), y: -self.padding)) // 三角の先っちょ。ここから時計回りに三角形を描く
+                    }
+                    .fill(Color.white)
+                    .frame(height: 16)
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
